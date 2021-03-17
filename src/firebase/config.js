679fc6c44367677f12c8ff24f4727eba5cb9c29e
gitcoder-23 <prabhatece21@gmail.com>
2021-03-17 11:51:37 +0530

    ✨ Make index by "productCategory" "createdDate" F

---
+++ src/firebase/config.js
@@ -0,0 +1,10 @@
+// firebase config
+export const firebaseConfig = {
+    apiKey: "AIzaSyAo5IYQemqfAyfh5LoL4eU2-Eo-IGlxjys",
+    authDomain: "ecommerce-website-cf450.firebaseapp.com",
+    projectId: "ecommerce-website-cf450",
+    storageBucket: "ecommerce-website-cf450.appspot.com",
+    messagingSenderId: "401982297026",
+    appId: "1:401982297026:web:4c1a44f52d60358691fc5b"
+
+};
---
+++ src/redux/Products/products.types.js
@@ -0,0 +1,12 @@
+const productsTypes = {
+  ADD_NEW_PRODUCT_START: 'ADD_NEW_PRODUCT_START',
+  // fetch product to dashboard
+  FETCH_PRODUCTS_START: 'FETCH_PRODUCTS_START',
+  // to store pro to redux store
+  SET_PRODUCTS: 'SET_PRODUCTS',
+  DELETE_PRODUCT_START: 'DELETE_PRODUCT_START',
+  FETCH_PRODUCT_START: 'FETCH_PRODUCT_START',
+  SET_PRODUCT: 'SET_PRODUCT',
+};
+
+export default productsTypes;
