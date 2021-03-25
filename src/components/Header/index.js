4bac8488c33da4cf9c7c6c10b2937e4a82bf1269
gitcoder-23 <prabhatece21@gmail.com>
2021-03-25 14:25:37 +0530

    🚑

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -58,7 +58,7 @@ const Header = props => {
             <li>
               <Link to="/cart">
                 Your Cart ({totalNumCartItems})
-                <i class="fas fa-shopping-basket"></i>
+                <i className="fas fa-shopping-basket" alt="Cart" title="Cart"></i>
               </Link>
             </li>
 
@@ -66,13 +66,13 @@ const Header = props => {
               <li key={1}>
                 <Link to="/dashboard">
                   My Account
-                  <i class="fas fa-user-circle"></i>
+                  <i className="fas fa-user-circle" alt="My Account" title="My Account"></i>
                 </Link>
               </li>,
               <li key={2}>
                 <span onClick={() => signOut()}>
                   LogOut
-                  <i class="fas fa-sign-out-alt"></i>
+                  <i className="fas fa-sign-out-alt" alt="LogOut" title="LogOut"></i>
                 </span>
               </li>
             ]}
@@ -86,7 +86,7 @@ const Header = props => {
               <li key={2}>
                 <Link to="/login">
                   Login
-                  <i class="fas fa-user-circle"></i>
+                  <i className="fas fa-user-circle" alt="Login" title="Login"></i>
                 </Link>
               </li>
             ]}
